b24bec5d1a404879b14e62f51b49fe9a9a40f3f3
ElhamRazmjooy <elhamrazmjooy@gmail.com>
2026-08-25 23:04:05 +0330

    C# Updated

---
+++ C#/PassByReference/PassByReference/Program.cs
@@ -0,0 +1,25 @@
+﻿//ref
+//void Increase(ref int number) => number++;
+//int x = 10;
+//Increase(ref x);
+//Console.WriteLine(x);
+
+//without ref
+//void Increase(int number) => number++;
+//int x = 10;
+//Increase(x);
+//Console.WriteLine(x);
+
+//out
+//bool TryParse(string text, out int result) => int.TryParse(text, out result);
+//if (int.TryParse("349", out int num))
+//    Console.WriteLine(num);
+//if (TryParse("Hello", out int num2))
+//    Console.WriteLine(num2);
+
+//in
+//void Print(in int num) => Console.WriteLine(num);
+//int number = 30;
+//Print(number);
+
+Console.ReadLine();
